4eb69f6af3d6c633fc39f3cef6691a239320dd6b
TeoWeiShan <s10205156@connect.np.edu.sg>
2023-08-15 21:46:48 +0800

    Initial commit

---
+++ src/context/alertContext.js
@@ -0,0 +1,26 @@
+import { createContext, useContext, useState } from 'react';
+
+const AlertContext = createContext(undefined);
+
+export const AlertProvider = ({ children }) => {
+  const [alertState, setAlertState] = useState({
+    isOpen: false,
+    type: 'success',
+    message: '',
+  });
+
+  return (
+    <AlertContext.Provider
+      value={{
+        ...alertState,
+        onOpen: (type, message) =>
+          setAlertState({ isOpen: true, type, message }),
+        onClose: () => setAlertState({ isOpen: false, type: '', message: '' }),
+      }}
+    >
+      {children}
+    </AlertContext.Provider>
+  );
+};
+
+export const useAlertContext = () => useContext(AlertContext);
